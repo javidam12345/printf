9036711b18f1c908771079f26c97ef072c5a2ae4
javi <javiaragoneses1@outlook.com>
2023-10-19 12:55:09 +0200

    a

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -1,10 +1,11 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
+#include <stdarg.h>
 
 int main();
 
 int ft_printf(const char *format, ...);
 
-
+void analize_string(const char* format, va_list argp);
 
 #endif
